8c09690e8224993557ca63484ae4fb149225c1fb
Samuel Holmez <samuelholmez@Samuels-MacBook-Air.local>
2023-06-13 15:55:40 -0400

    first commit

---
+++ doitagain.js
@@ -0,0 +1,9 @@
+const movies = ["The Shawshank Redemption", "Pulp Fiction", "The Godfather", "Fight Club", "Inception"];
+
+const arrayLog = (stuff)=> {
+    for(let i=0; i < stuff.length; i++){
+        console.log(stuff[i]);
+    }
+}
+
+arrayLog(movies);
---
+++ index.js
@@ -0,0 +1,17 @@
+const loopy = number => {
+
+for(let i = 0; i < number; i++){
+    console.log(i);
+}
+}
+
+function fingerLoop(){
+    const totalfinger = 10;
+
+for(let i = 0; i < totalfinger; i++){
+    
+    let sentence =  `i have ${i+1} fingers`
+    console.log(sentence);
+}}
+
+loopy(11);
